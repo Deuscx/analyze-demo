ea3ff629565ae64c3e8503899a15b602c0897ab7
deuscx <1475453714@qq.com>
2025-05-22 21:35:42 +0800

    chore: don't use router view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <main font-sans p="x-4" text="center gray-700 dark:gray-200">
-    <RouterView />
+    <t-tabs :default-value="1">
+      <t-tab-panel :value="1" label="模块1">
+        <ModuleOne />
+      </t-tab-panel>
+      <t-tab-panel :value="2" label="模块2">
+        <ModuleTwo />
+      </t-tab-panel>
+    </t-tabs>
   </main>
 </template>
